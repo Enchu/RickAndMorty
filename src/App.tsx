a8e80d9d7b3b89758ee7c43659c0a8de7b26aff3
Enchu <aleck.germanov@yandex.ru>
2025-12-09 20:44:58 +0300

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>123</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
